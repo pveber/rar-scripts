6515733680eef6766f9f4c0f4f504342bbd7949b
pveber <pveber@free.fr>
2011-07-27 20:04:17 +0200

    [FIX] le script de téléchargement du gtf ensembl incorrect

--- ensembl.ml
+++ ensembl.ml
@@ -3,12 +3,12 @@ open Guizmin
 let string_of_org = function
     `mouse -> "mouse"
 let transcript_url = function
-  `mouse -> "ftp://ftp.ensembl.org/pub/current_gtf/mus_musculus/Mus_musculus.NCBIM37.61.gtf.gz"
+  `mouse -> "ftp://ftp.ensembl.org/pub/current_gtf/mus_musculus/Mus_musculus.NCBIM37.63.gtf.gz"
 
 let transcripts x = target 
   [ "mkdir -p $@_download" ;
     sp "cd $@_download && wget %s" (transcript_url x) ;
     "cd $@_download && gunzip *.gz" ;
-    "cd $@_download && gawk '\\{print \"chr\"$0\\}' *.gtf | sed 's/chrMT/chrM/g' | grep -v 'ENSMUST00000127664' > $@" ;
+    "D=${PWD} && cd $@_download && gawk '{print \"chr\"$0}' *.gtf | sed 's/chrMT/chrM/g' | grep -v 'ENSMUST00000127664' > ${D}/$@" ;
     "rm -rf $@_download" ]
   []
