3dcfc123fdb969296fbb9b62926d12f97accdd53
pveber <philippe.veber@gmail.com>
2011-08-10 15:23:18 +0200

    [ADD] accomodate with new format for justin clustering
    - also improved the figure for cluster homogeneity

--- justin_chipseq_clustering.ml
+++ justin_chipseq_clustering.ml
@@ -3,15 +3,7 @@ open Printf
 open Oregon
 open Target.Infix
 
-let archive = Misc.unvac 
-  ~host:"titan.ibisc.fr" 
-  ~adr:"vac:bec7ce44a0702d261c56f88e31ec2dd6a1128f0d"
-  (`directory ())
-
-let prepare_archive () = 
-  Sle.sh "cd %s && find . -name '*.gz' -exec gunzip {} \\;" archive#path
-
-(* Fichier des pics sélectionnés *)
+(* sortie par justin *)
 type col selected_peak = {
   chr : string ;
   st : int ;
@@ -19,32 +11,34 @@ type col selected_peak = {
   t0 : float ;
   t2 : float ;
   t24 : float ;
-  t48 : float
+  t48 : float ;
+  cl2 : int ;
+  cl3 : int ;
+  cl4 : int ;
+  cl5 : int ;
+  cl6 : int ;
+  cl7 : int ;
+  cl8 : int ;
+  cl9 : int ;
+  cl10 : int 
 }
 
-let selected_peaks =
-  Target.in_directory
-    archive "clustering/res/chipseq/data.txt"
-    Tsv.({ has_header = true ;
-	   parse = Selected_peak.of_array })
+let s_of_tf = function `rar -> "rar" | `rxr -> "rxr"
+let s_of_cm = function `spectral -> "spectral" | `hsic -> "hsic"
 
-
-
-(*
-let ( !! ) = Lazy.force
-
-let nbclusters = lazy (
-  shell <:sprint<ls $s:archive#path$/clustering/$s:assert false$/*.??? | wc -l>>
+let get_cluster n p = 
+  try
+  (Selected_peak.to_array p).(Selected_peak.Index.cl2 + n - 2)
   |> int_of_string
-)
-*)
-    
-let cluster_assignment tf n =
-  let tf = match tf with `rar -> "rar" | `rxr -> "rxr" in
-  (Target.in_directory archive (sprintf "clustering/res/chipseq/hsic/%s/%s-%d" tf tf n) ())#path
-  |> File.lines_of
-  |> Enum.skip 1
-  |> Enum.map int_of_string
+  with _ -> failwith "smartass"
+
+let clustering_path tf cm = 
+  sprintf "extdata/justin/clustering/chipseq/%s-%s.txt" (s_of_tf tf) (s_of_cm cm)
+
+let load_peaks tf cm = 
+  File.lines_of (clustering_path tf cm)
+  |> Enum.map (Sle.SleString.split '\t')
+  |> Enum.map Selected_peak.of_array
   |> Array.of_enum
 
 let count_nb_clusters c =
@@ -53,26 +47,26 @@ let count_nb_clusters c =
   |> PMap.enum 
   |> Enum.hard_count
 
-let cluster_homogeneity_fig tf n = Target.F.make
-  (object
-     method id = "Justin_chipseq_clustering.cluster_homogeneity_fig[r1"
-     method deps = [] ++ archive
-     method build path =
-       let rp = R.make ()
-       and cluster_assignment = cluster_assignment tf n 
-       and peaks = Tsv.enum selected_peaks |> Array.of_enum in
-       let sn = int_of_float (ceil (sqrt (float n))) in
-       R.pdf rp path ;
-       R.c rp "par(mfrow=c(%d,%d))" sn sn ;
-       for j = 1 to n do
-	 R.c rp "plot(c(1,3),c(-5,5),type='n')" ;
-	 Sle.SleArray.filteri (fun i _ -> cluster_assignment.(i) = j) peaks 
-	 |> Array.iter (fun p -> Selected_peak.(R.c rp "lines(c(1,2,3),c(%f,%f,%f),lwd=0.3)" p.t2 p.t24 p.t48)) ;
-       done ;
-       R.devoff rp ;
-       R.close rp
-     method ty = `pdf
-   end)
+let cluster_homogeneity_fig tf cm n path = 
+  let rp = R.make () 
+  and peaks = load_peaks tf cm in
+  let cluster_assignment = Array.map (get_cluster n) peaks in
+  R.pdf rp path ;
+  for j = 0 to n - 1 do
+    R.c rp "plot(c(0,48),c(-5,5),type='n')" ;
+    Sle.SleArray.filteri (fun i _ -> cluster_assignment.(i) = j) peaks 
+    |> Array.iter (fun p -> Selected_peak.(R.c rp "lines(c(0,2,24,48),c(%f,%f,%f,%f),lwd=0.3)" p.t0 p.t2 p.t24 p.t48)) ;
+  done ;
+  R.devoff rp ;
+  R.close rp
+
+(* let cluster_homogeneity_fig_target tf cm n = Target.F.make *)
+(*   (object *)
+(*      method id = "Justin_chipseq_clustering.cluster_homogeneity_fig[r2]" *)
+(*      method deps = [] ++ archive *)
+(*      method build path = cluster_homogeneity_fig tf cm n path *)
+(*      method ty = `pdf *)
+(*    end) *)
 
 (* let filter_by_cluster a i =  *)
 (*   let clusters = clusters () in *)
