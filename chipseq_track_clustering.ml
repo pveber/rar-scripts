d94b03ded462f2079f1e552084e8806fa8f454f5
pveber <philippe.veber@gmail.com>
2011-08-16 14:41:35 +0200

    [ADD] clustering of ES chipseqs

--- chipseq_track_clustering.ml
+++ chipseq_track_clustering.ml
@@ -42,7 +42,7 @@ let tracks_of_samples samples =
     (fun s -> 
        B.B.Chipseq.(string_of_sample s,
 		    (macs_peaks |- Macs.filter ~pvalue:1e-9 |- selection_of_bed) s))
-    samples
+    (Array.of_list samples)
 
 type col es_tsv = {
   loc : Location ;
@@ -55,7 +55,41 @@ let es_peaks path =
     ("ftp://ftp.ncbi.nlm.nih.gov/pub/geo/DATA/supplementary/samples/GSM288nnn/" ^ path)
     Tsv.({has_header = false ; parse = Es_tsv.of_array })
 
+let nanog_peaks = es_peaks "GSM288345/GSM288345_ES_Nanog.txt.gz"
 let oct4_peaks = es_peaks "GSM288346/GSM288346_ES_Oct4.txt.gz"
+let sox2_peaks = es_peaks "GSM288347/GSM288347_ES_Sox2.txt.gz"
+let smad1_peaks = es_peaks "GSM288348/GSM288348_ES_Smad1.txt.gz"
+let e2f1_peaks = es_peaks "GSM288349/GSM288349_ES_E2f1.txt.gz"
+let tcfcp2l1_peaks = es_peaks "GSM288350/GSM288350_ES_Tcfcp2l1.txt.gz"
+let zfx_peaks = es_peaks "GSM288352/GSM288352_ES_Zfx.txt.gz"
+let stat3_peaks = es_peaks "GSM288353/GSM288353_ES_Stat3.txt.gz"
+let klf4_peaks = es_peaks "GSM288354/GSM288354_ES_Klf4.txt.gz"
+let c_myc_peaks = es_peaks "GSM288356/GSM288356_ES_c-Myc.txt.gz"
+let n_myc_peaks = es_peaks "GSM288357/GSM288357_ES_n-Myc.txt.gz"
+let p300_peaks = es_peaks "GSM288359/GSM288359_ES_p300.txt.gz"
+let suz12_peaks = es_peaks "GSM288360/GSM288360_ES_Suz12.txt.gz"
+
+
+let es_selection x = 
+  Tsv.enum x 
+  |> Enum.map (fun x -> Location.(relmove x.loc (-200) (200)))
+  |> RarGenome.Selection.of_locations
+
+let es_tracks () = [|
+  "Nanog", es_selection nanog_peaks ;
+  "Oct4", es_selection oct4_peaks ;
+  "Sox2", es_selection sox2_peaks ;
+  "Smad1", es_selection smad1_peaks ;
+  "E2f1", es_selection e2f1_peaks ;
+  "Tcfcp2l1", es_selection tcfcp2l1_peaks ;
+  "Zfx", es_selection zfx_peaks ;
+  "Stat3", es_selection stat3_peaks ;
+  "Klf4", es_selection klf4_peaks ;
+  "c-Myc", es_selection c_myc_peaks ;
+  "n-Myc", es_selection n_myc_peaks ;
+  "p300", es_selection p300_peaks ;
+  "Suz12", es_selection suz12_peaks ;
+|]
 
 let sample_hclust_plot tracks path = 
   let rp = R.make () 
