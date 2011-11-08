90c2a8d98861a3cd75cc2e7c85dcf4e21a8a4019
pveber <philippe.veber@gmail.com>
2011-11-08 17:06:23 +0100

    commands to calculate annotations

---
+++ annotations.ml
@@ -0,0 +1,21 @@
+let chipseq_design = 
+  List.concat [
+    List.map (fun x -> x, `Input_1) PanRAR_regions.conditions ;
+    List.map (fun x -> x, `Mendoza_input) Rarg_regions.conditions
+  ]
+    
+let _ = Chipseq_annotation.(
+  tsv
+    "RARg_regions_chipseq_annotation.tsv"
+    (make 
+       chipseq_design
+       Rarg_regions.gerard_selection#value)
+);;
+
+let _ = Chipseq_annotation.(
+  tsv
+    "PanRAR_regions_chipseq_annotation.tsv"
+    (make 
+       chipseq_design
+       PanRAR_regions.gerard_selection#value)
+);;
--- panRAR_regions.mli
+++ panRAR_regions.mli
@@ -3,5 +3,6 @@
 open Genome
 open Oregon.Target
 
+val conditions : Chipseq.sample list
 val all_of_them : Location.t array value
 val gerard_selection : Location.t array value
