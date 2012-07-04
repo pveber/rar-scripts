b7cf800d064a344a4bebb2e52236d46544116392
pveber <philippe.veber@gmail.com>
2012-07-04 14:38:37 +0200

    un histogramme plus joli pour la pos relative des pics % TSS le plus proche
    et une façon plus pratique de bosser que de faire des bindings ocaml-r
    à chaque fois que j'ai besoin de quelque chose.

--- src/app_region_assoc.ml
+++ src/app_region_assoc.ml
@@ -19,21 +19,55 @@ let r_distance_breaks =
 let rel_pos_bregions_fig_path = 
   "relative_position_of_bindings_wrt_closest_tss.pdf"
 
-let app = Guizmin.d0 ("rar.app_region_assoc[r1]", []) (fun path ->
+let rel_pos_bregions_fig_script = "\
+rel_pos_bregions_fig <- function(path,
+"
+
+let job_script = "\
+job <- function(path, closest_tss_rel_pos) {
+  breaks <- c(-1000000,-100000,-50000,-20000,-10000,-5000,-2000,-1000,0,1000,2000,5000,10000,20000,50000,100000,1000000)
+  breaks_ranges <- abs(breaks[1:length(breaks) - 1] - breaks[2:length(breaks)])
+  breaks_legend <- c('[-1000000;-100000]','[-100000;-50000]','[-50000;-20000]','[-20000;-10000]','[-10000;-5000]','[-5000;-2000]','[-2000;-1000]','[-1000;0]','[0;1000]','[1000;2000]','[2000;5000]','[5000;10000]','[10000;20000]','[20000;50000]','[50000;100000]','[100000;1000000]')
+  rel_pos_hist <- hist(closest_tss_rel_pos[abs(closest_tss_rel_pos) < 1000000], breaks,plot=F)
+  pdf(paste(path,'relative_position_of_bindings_wrt_closest_tss.pdf',sep='/'))
+  barplot(rel_pos_hist$counts / breaks_ranges, names.arg=breaks_legend,  las=2, cex.names=0.5)
+  dev.off()
+
+}
+"
+let job = 
+  let _ = R.eval_string job_script in 
+  let symbol = R.symbol "job" in
+  fun path closest_tss_rel_pos ->
+    R.eval symbol [
+      R.arg R.string path ;
+      R.arg R.floats closest_tss_rel_pos ;
+    ]
+  |> ignore
+
+let debug_bck_script = "\
+debug_bck <- function(path, closest_tss_rel_pos) {
+  backup <- list(closest_tss_rel_pos = closest_tss_rel_pos)
+  save(backup,file=paste(path,'debug.Rd',sep='/'))
+}
+"
+let debug_bck = 
+  let _ = R.eval_string debug_bck_script in 
+  let symbol = R.symbol "debug_bck" in
+  fun path closest_tss_rel_pos ->
+    R.eval symbol [
+      R.arg R.string path ;
+      R.arg R.floats closest_tss_rel_pos ;
+    ]
+  |> ignore
+
+let app = Guizmin.d0 ("rar.app_region_assoc[r5]", []) (fun path ->
   ignore (Sys.command ("mkdir -p " ^ path)) ;
   (*  *)
   let closest_gene_for_each_bregion_list = List.of_enum (closest_gene_for_each_bregion ()) in
-  RgrDevices.pdf (Filename.concat path rel_pos_bregions_fig_path) ;
-  Rgraphics.hist 
-    ~breaks:r_distance_breaks
-    ~main:"Relative position of binding regions wrt to closest TSS"
-    ~xlab:"Relative position (in bp)"
-    ~ylab:"Frequence"
-    (closest_gene_for_each_bregion_list
-       |> List.map (Tuple3.third |- float_of_int)
-       |> List.filter (fun x -> abs_float x < 100000.)
-       |> R.floats) |> ignore ;
-  RgrDevices.dev_off () ;
+  let positions = List.map (Tuple3.third |- float_of_int) closest_gene_for_each_bregion_list in
+  job path positions ;
+  debug_bck path positions 
 
 )
 
